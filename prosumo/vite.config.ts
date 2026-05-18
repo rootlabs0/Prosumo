1d8085207ae2649ee5440c9dfcacdd126f49acc8
Viktor Zatloukal <vikzatlouk@gmail.com>
2026-05-18 23:54:34 +0200

    architecture redesign

--- prosumo/vite.config.ts
+++ prosumo/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/Prosumo/' : '/',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
 })
